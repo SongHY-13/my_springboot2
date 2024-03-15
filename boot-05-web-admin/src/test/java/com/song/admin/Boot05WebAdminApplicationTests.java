ba0d45e47140888a655d779ac2fbea5114704cdb
SongHY-13 <62045743+SongHY-13@users.noreply.github.com>
2024-03-15 18:15:36 +0800

    数据库场景引入1

--- boot-05-web-admin/src/test/java/com/song/admin/Boot05WebAdminApplicationTests.java
+++ boot-05-web-admin/src/test/java/com/song/admin/Boot05WebAdminApplicationTests.java
@@ -1,13 +1,21 @@
 package com.song.admin;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+@Slf4j
 @SpringBootTest
 class Boot05WebAdminApplicationTests {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Test
     void contextLoads() {
+        Long l = jdbcTemplate.queryForObject("select count(*) from t_user", Long.class);
+        log.info("表中记录总数：{}", l);
     }
 
 }
